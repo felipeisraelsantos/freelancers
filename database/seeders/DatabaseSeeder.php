4633903f2957fa5f8df3d6bf6a858aee33f73e3f
felipeisraelsantos <felipe.israel@aluno.ifsp.edu.br>
2024-10-12 13:35:14 -0300

    feat: alterado seeder para criaçao de usuarios e projetos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)
+            ->count(200)
+            ->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+            // User::query()->inRandomOrder()->limit(10)->get()
+            // ->each(function (User $u) {
+            //     $project = Project::factory()->create(['created_by' => $u->id]);
+            //     Proposal::factory()->count(random_int(4, 45))->create(['project_id' => $project->id]);
+            // });
+
+            User::query()->inRandomOrder()->limit(10)->get()
+                ->each(fn (User $u) => $project = Project::factory()->create(['created_by' => $u->id]));
     }
 }
